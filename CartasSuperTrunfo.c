0d63d4e512acb9a8c08ea5bf38ae2b7b6741035a
Cristhyan Capetini <capetini.cris@gmail.com>
2025-03-26 10:08:58 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,47 +13,54 @@ int main() {
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
-    //dados da primeira cidade (rio de janeiro)
-    int Codigo1 =1;
-    char Nome1[] = "Rio de Janeiro";
-    long long Populacao1 = 6748000;
-    float Area1 = 1200.00;
-    double Pib1 = 35000000000.00;
-    int Pontos_Turisticos1 = 50;
+ // Dados da Carta 1 (Rio de Janeiro)
+    char codigo1[] = "A01";
+    char nome_cidade1[] = "Rio de Janeiro";
+    long long populacao1 = 6748000;
+    float area1 = 1200.00;
+    double pib1 = 350.00; // PIB em bilhões
+    int pontos_turisticos1 = 50;
+    char estado1 = 'A'; // Estado A para Carta 1
 
-    //dados da segunda cidade (belo horizonte)
-    int Codigo2 = 2;
-    char Name2[] = "Belo Horizonte";
-    long long Populacao2 = 2521000;
-    float Area2 = 331.40;
-    double Pib2 = 90000000000.00;
-    int Pontos_Turisticos2 = 30;
+
+    // Dados da Carta 2 (Belo Horizonte)
+    char codigo2[] = "B02";
+    char nome_cidade2[] = "Belo Horizonte";
+    long long populacao2 = 2521000;
+    float area2 = 331.40;
+    double pib2 = 90.00; // PIB em bilhões
+    int pontos_turisticos2 = 30;
+    char estado2 = 'B'; // Estado B para Carta 2
     
    
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
     
-    //dados do Rio de Janeiro
+ // Exibição dos Dados das Cartas
 
-    
-    printf("Cidade 1:\n");
-    printf("Codigo: %d\n", codigo1);
-    printf("Nome: %s\n", nome1);
-    printf("Populacao: %lld\n", populacao1);
-    printf("Area: %.2f km2\n", area1);
-    printf("Pib: R$ %.2lf\n", pib1);
-    printf("Numero de Pontos Turiticos: %d\n\n", pontos_turisticos1);
+    // Dados do Rio de Janeiro
+    printf("Carta 1:\n\n");
+    printf("Estado : %c\n", estado1);
+    printf("Código : %s\n", codigo1);
+    printf("Nome da Cidade : %s\n", nome_cidade1);
+    printf("População : %lld\n", populacao1);
+    printf("Área : %.2f km²\n", area1);
+    printf("PIB : %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos : %d\n\n", pontos_turisticos1);
 
     //Dados de Belo Horizonte
 
-    printf("Cidade 2:\n");
-    printf("Codigo: %d\n", codigo2);
-    printf("Nome: %s\n", nome2);
-    printf("Populacao: %lld\n", populacao2);
-    printf("Area: %.2f km2\n", area2);
-    printf("Pib: R$ %.2lf\n", pib2);
-    printf("Numero de Pontos Turisticos: %d\n", pontos_turisticos2);
+    
+    printf("Carta 2:\n\n");
+    printf("Estado : %c\n", estado2);
+    printf("Código : %s\n", codigo2);
+    printf("Nome da Cidade : %s\n", nome_cidade2);
+    printf("População : %lld\n", populacao2);
+    printf("Área : %.2f km²\n", area2);
+    printf("PIB : %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos : %d\n", pontos_turisticos2);
+
 
     return 0;
 }
